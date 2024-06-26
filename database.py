58607da9695a1af6b2e760675c7033bf70150b9d
Lucas de Lima Bica <delimabica@gmail.com>
2024-06-27 00:27:33 +0100

    Database <-> Server

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
+import json
 
-Base = declarative_base() # -> Returns a base class to me define declarative classes (tables)
+Base = declarative_base()  # Returns a base class to define declarative classes (tables)
 
 class User(Base):
     __tablename__ = 'users'
@@ -21,14 +22,89 @@ class User(Base):
     # Method to check info about the user (like the __str__ method)
     def __repr__(self):
         return f"<User(id={self.id}, username='{self.username}', password='{self.password}', creation_date='{self.creation_date}')>"
-    
 
-engine = create_engine("sqlite:///database.db", echo=True) # Create a connection to the SQLite database
-Base.metadata.create_all(bind=engine) # -> Creates the tables (only if they don't exist)
+class Document(Base):
+    __tablename__ = 'documents'
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String, nullable=False)
+    user_owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    expiration_date = Column("expiration_date", DateTime, nullable=True)
+    owner = relationship("User", back_populates="documents")
+
+    # Table Creation
+    def __init__(self, name, owner, expiration_date=None):
+        self.name = name
+        self.owner = owner
+        self.expiration_date = expiration_date
+
+    # Method to check info about the document (like the __str__ method)
+    def __repr__(self):
+        return f"<Document(id={self.id}, name='{self.name}', owner='{self.owner.username}', expiration_date='{self.expiration_date}')>"
+
+# Add the relationship from the User side
+User.documents = relationship("Document", order_by=Document.id, back_populates="owner")
+
+engine = create_engine("sqlite:///database.db", echo=True)  # Create a connection to the SQLite database
+Base.metadata.create_all(bind=engine)  # Creates the tables (only if they don't exist)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-user = User("john_doe", "password123")  # Create a new user
-session.add(user)
-session.commit()
+# Check if 'admin' user exists before adding
+if not session.query(User).filter_by(username='admin').first():
+    admin_user = User("admin", "87654321")
+    session.add(admin_user)
+    session.commit()
+
+# Fetch all users
+# users_list = session.query(User).all()
+# for user in users_list:
+#     print(user)
+
+# Create and add a document for the first user in the list
+# if users_list:
+#     drivers_license = Document("Drivers License", users_list[0])
+#     session.add(drivers_license)
+#     session.commit()
+
+# Uncomment the following lines to filter users with insecure passwords
+# not_secured_users = session.query(User).filter(User.password == '87654321').all()
+# for user in not_secured_users:  # User's type = class User
+#     print(f"{user.username} hasn't a secure password.")
+
+# Uncomment the following lines to add a new user
+# new_user = User("suporte", "87654321")  # Create a new user
+# session.add(new_user)  # Add the new user to the session
+# session.commit()  # Commit the changes to the database
+
+# Print the admin's password
+# admin_user = session.query(User).filter_by(username='admin').first()
+# if admin_user:
+#     print(f"The admin password is {admin_user.password}")
+# 
+# # Print the name of the document owned by the admin
+# admin_document = session.query(Document).filter_by(user_owner_id=admin_user.id).first()
+# if admin_document:
+#     print(f"The admin document is {admin_document.name}")
+
+def get_users():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    users = session.query(User).all()
+    users_dict = [
+        {
+            "id": user.id,
+            "username": user.username,
+            "password": user.password,
+            "creation_date": user.creation_date.isoformat()
+        }
+        for user in users
+    ]
+    
+    users_json = json.dumps(users_dict, indent=4)
+    return users_json
+    # session.query(User).filter(User.id == id).update({"username": username})
+
+print(get_users())
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import requests
+import database
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ def home():
 @app.route('/courses', methods=['GET'])
 def get_courses():
     return "Your Courses!"
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return database.get_users()
 
 if __name__ == '__main__':
     app.run(port=443)
