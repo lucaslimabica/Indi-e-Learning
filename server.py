6481dc01c7b8ced7ba3a413b1fb855c1a073f454
Lucas de Lima Bica <delimabica@gmail.com>
2024-06-27 00:42:15 +0100

    Server with Getter Specific User

--- server.py
+++ server.py
@@ -15,5 +15,9 @@ def get_courses():
 def get_users():
     return database.get_users()
 
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    return database.get_user(id=id)
+
 if __name__ == '__main__':
     app.run(port=443)
