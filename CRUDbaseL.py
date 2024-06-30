8062057f2532b95402250d7b0af608576c5b5912
Lucas de Lima Bica <delimabica@gmail.com>
2024-06-30 20:35:46 +0100

    Courses

--- CRUDbaseL.py
+++ CRUDbaseL.py
@@ -53,3 +53,62 @@ def create_user(userjson: dict):
         return True, user
     except sqlite3.OperationalError as e:
         return False, f"Error: Maybe the Database isn't available, Complete Error: {str(e)}"
+
+def get_courses() -> list[tuple]:
+    conn, cursor = get_connection(DATABASE)
+    try:
+        cursor.execute("SELECT * FROM courses")
+        courses = cursor.fetchall()
+        conn.commit()
+        conn.close()
+        if courses == "None":
+            return False, "Error: Courses Table is Empty"
+    except sqlite3.OperationalError:
+        return False, f"Error: This Table my be not exist, Complete Error: {sqlite3.OperationalError}"
+    return True, f"{courses}"
+
+def get_course(id: int, name: str=None) -> tuple:
+    conn, cursor = get_connection(DATABASE)
+    try:
+        if id:
+            cursor.execute("SELECT * FROM courses WHERE id =?", (id,))
+        elif name:
+            cursor.execute("SELECT * FROM courses WHERE username =?", (name,))
+        course = cursor.fetchone()
+        conn.commit()
+        conn.close()
+        if course == "None":
+            return False, "Error: ID or Name did not exist"
+        return True, f"{course}"
+    except sqlite3.OperationalError:
+        return False, f"Error: Maybe the Database isnt avaliable, Complete Error: {sqlite3.OperationalError}"
+
+def create_course(coursejson: dict):
+    conn, cursor = get_connection(DATABASE)
+
+    try:
+        # Inserir usuário na tabela users
+        cursor.execute("INSERT INTO courses (name, description, subscriptions, value, professor_id) VALUES (:name, :description, :subscriptions, :value, :professor_id)",
+                       {"name": coursejson["name"],
+                        "description": coursejson["description"],
+                        "subscriptions": coursejson["subscriptions"],
+                        "value": coursejson["value"],
+                        "professor_id": coursejson["professor_id"]})
+        conn.commit()
+
+        # Selecionar o usuário recém-criado
+        cursor.execute("SELECT * FROM courses WHERE name = ? AND value = ?", (coursejson["name"], coursejson["value"]))
+        user = cursor.fetchone()
+
+        conn.close()
+        return True, user
+    except sqlite3.OperationalError as e:
+        return False, f"Error: Maybe the Database isn't available, Complete Error: {str(e)}"
+    
+create_course({
+    "name": "The Light Saber 101 Basics Vol.3",
+    "description": "How to use a basic light saber", 
+    "subscriptions": 2,
+    "value": 8100,
+    "professor_id": 1 
+})
--- server.py
+++ server.py
@@ -1,30 +1,39 @@
 from flask import Flask, jsonify, request
 import CRUDbaseL
-
+import json
 
 app = Flask(__name__)
 
 STR_SUCESS = "Sucess: True, data: "
 STR_ERROR = "Sucess: False, error: "
 
+def validate_json(json_string, required_keys) -> bool:
+    """
+    Valida o JSON assegurando que nele estão todas as necessárias chaves.
+
+    Args:
+        json_string (str): O JSON como uma string.
+        required_keys (list): Array de todas as chaves do JSON.
+
+    Returns:
+        bool: True se todas as chaves necessárias estão presentes, caso contrário, False.
+    """
+    try:
+        # JSON string -> dict
+        data = json.loads(json_string)
+    except:  # noqa: E722
+        return False
+
+    # Verify if do all required keys are in the json 
+    for key in required_keys:
+        if key not in data:
+            return False
+
+    return True
+
 @app.route('/')
 def home():
     return "Hello, World!"
-
-# @app.route('/courses', methods=['GET'])
-# def get_courses():
-#     pass
-#     #return "Your Courses!"
-# 
-# @app.route('/courses/<int:id>', methods=['GET'])
-# def get_courseById(id):
-#     pass
-#     #return CRUDbaseL.get_user(id=id)
-
-@app.route('/courses/<username>', methods=['GET'])
-def get_courseByUsername(username):
-    pass
-    #return CRUDbaseL.get_user(username=username)
 
 @app.route('/users', methods=['GET'])
 def get_users():
@@ -58,6 +67,14 @@ def get_userByUsername(username):
         return 200, jsonify(f"{STR_SUCESS} {data}")
     else:
         return jsonify(f"{STR_ERROR} {data}")
+
+@app.route('/courses', methods=['GET'])
+def get_courses():
+    pass
+
+@app.route('/courses/<username>', methods=['GET'])
+def get_courseByUsername(username):
+    pass
 
 if __name__ == '__main__':
     app.run(port=443)
