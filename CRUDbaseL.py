5ffa29a20d905acde4a4bd21b43f4263e7af0d0c
Lucas de Lima Bica <delimabica@gmail.com>
2024-07-06 17:05:42 +0100

    Update CRUDbaseL.py

--- CRUDbaseL.py
+++ CRUDbaseL.py
@@ -2,8 +2,8 @@ import sqlite3
 
 DATABASE = "databasel.db"
 
-def get_connection(DATABASE):
-    conn = sqlite3.connect(DATABASE)
+def get_connection(db_name):
+    conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
     return conn, cursor
 
@@ -31,60 +31,58 @@ def get_users() -> list[tuple]:
     try:
         cursor.execute("SELECT * FROM users")
         users = cursor.fetchall()
-        conn.commit()
-        conn.close()
         if not users:
             return "Error: Users Table is Empty"
     except sqlite3.OperationalError as e:
         return f"Error: This table may not exist, Complete Error: {e}"
+    finally:
+        conn.commit()
+        conn.close()
     return users
 
-
-def get_user(id: int, username: str=None) -> tuple:
+def get_user(id: int = None, username: str = None) -> tuple or str:
     """
-    Retrieve the first user's tuple from the 'users' table.
+    Retrieve a user from the 'users' table by ID or username.
+
+    ### Parameters
+    - id (int): The ID of the user.
+    - username (str): The username of the user.
 
     ### Return
-    A tuple whose contains:
-    (id, username, password)
+    A tuple containing user data (id, username, password).
 
     ### Example
     get_user(1)
     >>> (1, 'admin', '123456789')
 
-    get_user('user')
+    get_user(username='user')
     >>> (2, 'user', '987654321')
 
-    user = get_user(1)
-    print(user[2])
-    >>> '123456789'
-
     ### Error Handling
-    If the users table is empty, an error message is returned.
+    If the user does not exist, an error message is returned.
     If there is an operational error (e.g., the table does not exist), an error message is returned.
 
     ### Returns
-    - tuples: Tuple contains user data (id, username, password).
+    - tuple: User data (id, username, password).
     - str: Error message if an error occurs.
     """
     conn, cursor = get_connection(DATABASE)
     try:
-        if id:
-            cursor.execute("SELECT * FROM users WHERE id =?", (id,))
-        elif username:
-            cursor.execute("SELECT * FROM users WHERE username =?", (username,))
+        if id is not None:
+            cursor.execute("SELECT * FROM users WHERE id = ?", (id,))
+        elif username is not None:
+            cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
         user = cursor.fetchone()
+        if not user:
+            return "Error: ID or Username did not exist"
+    except sqlite3.OperationalError as e:
+        return f"Error: Maybe the Database isn't available, Complete Error: {e}"
+    finally:
         conn.commit()
         conn.close()
-        if user == "None":
-            return "Error: ID or Username did not exist"
-        return user
-    except AttributeError:
-        return "Please check your parameters"
-    except sqlite3.OperationalError:
-        return f"Error: Maybe the Database isnt avaliable, Complete Error: {sqlite3.OperationalError}"
+    return user
 
-def create_user(userjson: dict) -> tuple:
+def create_user(userjson: dict) -> tuple or str:
     """
     Create a new user in the 'users' table and retrieve the newly created user.
 
@@ -103,79 +101,147 @@ def create_user(userjson: dict) -> tuple:
     ### Error Handling
     If there is an operational error (e.g., the database is not available), an error message is returned.
     """
-
     conn, cursor = get_connection(DATABASE)
     username = userjson["username"]
     password = userjson["password"]
     try:
-        cursor.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-                       {"username": userjson["username"],
-                        "password": userjson["password"]
-                       })
+        cursor.execute(
+            "INSERT INTO users (username, password) VALUES (:username, :password)",
+            {"username": username, "password": password}
+        )
         conn.commit()
-
         cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
         user = cursor.fetchone()
-
-        conn.close()
-        return user
-    except AttributeError:
-        return "Check your dict"
     except sqlite3.OperationalError as e:
-        return f"Error: Maybe the Database isn't available, Complete Error: {str(e)}"
+        return f"Error: Maybe the Database isn't available, Complete Error: {e}"
+    finally:
+        conn.close()
+    return user
 
 def get_courses() -> list[tuple]:
+    """
+    Retrieve all courses from the 'courses' table.
+
+    ### Return
+    A list of tuples, where each tuple contains:
+    (idc, name, description, subscriptions, value, professor_id)
+
+    ### Example
+    get_courses()
+    >>> [(1, 'Loren Ipsum', 'A Loren Ipsum course', 200, 120, 1),]
+
+    ### Error Handling
+    If the courses table is empty, an error message is returned.
+    If there is an operational error (e.g., the table does not exist), an error message is returned.
+
+    ### Returns
+    - list of tuples: Each tuple contains course data.
+    - str: Error message if an error occurs.
+    """
     conn, cursor = get_connection(DATABASE)
     try:
         cursor.execute("SELECT * FROM courses")
         courses = cursor.fetchall()
+        if not courses:
+            return "Error: Courses Table is Empty"
+    except sqlite3.OperationalError as e:
+        return f"Error: This table may not exist, Complete Error: {e}"
+    finally:
         conn.commit()
         conn.close()
-        if courses == "None":
-            return "Error: Courses Table is Empty"
-    except sqlite3.OperationalError:
-        return False, f"Error: This Table my be not exist, Complete Error: {sqlite3.OperationalError}"
     return courses
 
-def get_course(id: int, name: str=None) -> tuple:
+def get_course(id: int = None, name: str = None) -> tuple or str:
+    """
+    Retrieve a course from the 'courses' table by ID or name.
+
+    ### Parameters
+    - id (int): The ID of the course.
+    - name (str): The name of the course.
+
+    ### Return
+    A tuple containing course data (idc, name, description, subscriptions, value, professor_id).
+
+    ### Example
+    get_course(1)
+    >>> (1, 'Loren Ipsum', '...', 200, 120, 1)
+
+    get_course(name='Loren Ipsum')
+    >>> (1, 'Loren Ipsum', '...', 200, 120, 1)
+
+    ### Error Handling
+    If the course does not exist, an error message is returned.
+    If there is an operational error (e.g., the table does not exist), an error message is returned.
+
+    ### Returns
+    - tuple: Course data (idc, name, description, subscriptions, value, professor_id).
+    - str: Error message if an error occurs.
+    """
     conn, cursor = get_connection(DATABASE)
     try:
-        if id:
-            cursor.execute("SELECT * FROM courses WHERE id =?", (id,))
-        elif name:
-            cursor.execute("SELECT * FROM courses WHERE username =?", (name,))
+        if id is not None:
+            cursor.execute("SELECT * FROM courses WHERE idc = ?", (id,))
+        elif name is not None:
+            cursor.execute("SELECT * FROM courses WHERE name = ?", (name,))
         course = cursor.fetchone()
-        conn.commit()
-        conn.close()
-        if course == "None":
+        if not course:
             return "Error: ID or Name did not exist"
-        return course
-    except sqlite3.OperationalError:
-        return f"Error: Maybe the Database isnt avaliable, Complete Error: {sqlite3.OperationalError}"
-
-def create_course(coursejson: dict):
-    conn, cursor = get_connection(DATABASE)
-
-    try:
-        # Inserir usuário na tabela users
-        cursor.execute("INSERT INTO courses (name, description, subscriptions, value, professor_id) VALUES (:name, :description, :subscriptions, :value, :professor_id)",
-                       {"name": coursejson["name"],
-                        "description": coursejson["description"],
-                        "subscriptions": coursejson["subscriptions"],
-                        "value": coursejson["value"],
-                        "professor_id": coursejson["professor_id"]})
-        conn.commit()
-
-        # Selecionar o usuário recém-criado
-        cursor.execute("SELECT * FROM courses WHERE name = ? AND value = ?", (coursejson["name"], coursejson["value"]))
-        user = cursor.fetchone()
-
-        conn.close()
-        return user
     except sqlite3.OperationalError as e:
-        return f"Error: Maybe the Database isn't available, Complete Error: {str(e)}"
-    
-print(f"Getter Unique (username for example): {get_user(1)[1]}")
-print(f"General Getter: {get_users()}")
-print(f"Course Getter Unique: {get_courses()}")
-print(f"Getting Course ID: {get_course(2)}")
+        return f"Error: Maybe the Database isn't available, Complete Error: {e}"
+    finally:
+        conn.commit()
+        conn.close()
+    return course
+
+def create_course(coursejson: dict) -> tuple or str:
+    """
+    Create a new course in the 'courses' table and retrieve the newly created course.
+
+    ### Parameters
+    - coursejson (dict): A dictionary containing 'name', 'description', 'subscriptions', 'value', and 'professor_id' keys with corresponding values.
+
+    ### Return
+    - tuple: The newly created course as a tuple (idc, name, description, subscriptions, value, professor_id).
+    - str: Error message if an error occurs.
+
+    ### Example
+    coursejson = {
+        "name": "Math 101",
+        "description": "Basic Mathematics",
+        "subscriptions": 0,
+        "value": 100,
+        "professor_id": 1
+    }
+    create_course(coursejson)
+    >>> (1, 'Math 101', 'Basic Mathematics', 0, 100, 1)
+
+    ### Error Handling
+    If there is an operational error (e.g., the database is not available), an error message is returned.
+    """
+    conn, cursor = get_connection(DATABASE)
+    try:
+        cursor.execute(
+            "INSERT INTO courses (name, description, subscriptions, value, professor_id) VALUES (:name, :description, :subscriptions, :value, :professor_id)",
+            {
+                "name": coursejson["name"],
+                "description": coursejson["description"],
+                "subscriptions": coursejson["subscriptions"],
+                "value": coursejson["value"],
+                "professor_id": coursejson["professor_id"]
+            }
+        )
+        conn.commit()
+        cursor.execute("SELECT * FROM courses WHERE name = ? AND value = ?", (coursejson["name"], coursejson["value"]))
+        course = cursor.fetchone()
+    except sqlite3.OperationalError as e:
+        return f"Error: Maybe the database isn't available, Complete Error: {e}"
+    finally:
+        conn.close()
+    return course
+
+def examples():
+    print(f"Getter Unique: {get_user(1)}")
+    print(f"General Getter: {get_users()}")
+    print(f"Course Getter: {get_courses()}")
+    print(f"Getting Course ID: {get_course(2)}")
+
